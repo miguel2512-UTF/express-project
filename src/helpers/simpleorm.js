22c8d5174a90c1c09a3eeec0feeaba7505819d57
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2023-07-07 23:15:01 -0500

    SimpleORM: Some datatypes and init create entity

--- src/helpers/simpleorm.js
+++ src/helpers/simpleorm.js
@@ -1,24 +1,49 @@
 const db = require("../db")
 
+const DATA_TYPES = {
+    VARCHAR: 'VARCHAR(255)',
+    TEXT: 'TEXT',
+    INTEGER: 'INTEGER',
+    NUMERIC: 'NUMERIC',
+    REAL: 'REAL',
+    LONG: 'LONG',
+    BOOLEAN: 'BOOLEAN',
+    BLOB: 'BLOB',
+}
+
 class SimpleOrm {
 
-    constructor() {
-        this.init()
-    }
-
     init() {
+        let properties = this.getProperties()
+
+        properties = properties.map(el => {
+            return {
+                nullable: true,
+                constraints: {
+                    primary_key: false,
+                    auto_increment: false,
+                    unique: false
+                },
+                ...this[el]
+            }
+        })
+        console.log(properties);
+        const sql = properties.map(el => `${el.field} ${el.type} ${!el.nullable ? "NOT NULL" : ""} ${el.constraints["primary_key"] ? "PRIMARY KEY" : ""} ${el.constraints["auto_increment"] ? "AUTOINCREMENT" : ""} ${el.constraints["unique"] ? "UNIQUE" : ""}`).join(",")
+
+        console.log(sql);
         db.run(`
         CREATE TABLE IF NOT EXISTS ${this.constructor.name.toLowerCase()} (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            email text UNIQUE, 
-            password text
+            ${sql}
         )
         `, (err) => {
             if (err) {
-                console.log("Error to Create Table");
+                console.log("Error to Create Table " + err.message);
             }
         })
+    }
+
+    static getProperties() {
+        return Object.getOwnPropertyNames(this)
     }
 
     getProperties() {
@@ -41,20 +66,47 @@ class SimpleOrm {
     }
 }
 
-class User extends SimpleOrm {
+class Usuario extends SimpleOrm {
 
-    name = {
-        field: "name",
-        type: "string"
+    constructor() {
+        super()
+        this.init()
+    }
+
+    id = {
+        field: "id",
+        type: DATA_TYPES.INTEGER,
+        constraints: {
+            primary_key: true,
+            auto_increment: true,
+        }
+    }
+
+    firstname = {
+        field: "firstname",
+        type: DATA_TYPES.VARCHAR,
+        nullable: false
+    }
+
+    lastname = {
+        field: "lastname",
+        type: DATA_TYPES.VARCHAR
+    }
+
+    email = {
+        field: "email",
+        type: DATA_TYPES.VARCHAR,
+        constraints: {
+            unique: true
+        }
     }
 }
 
-const user = new User();
-console.log("USER PROPS", user);
+const user = new Usuario();
 
 async function getResult() {
-    const result = await User.findAll()
+    const result = await Usuario.findAll()
     console.log(result);
 }
 
-getResult()
+// getResult()
